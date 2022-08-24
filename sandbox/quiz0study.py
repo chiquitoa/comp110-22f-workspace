365d7e76d2f22363ce24dd9c76358dfbaed84eb9
Andre Chiquito <achiquit@email.unc.edu>
2022-08-24 13:59:55 -0400

    Quiz0 Study

---
+++ sandbox/quiz0study.py
@@ -0,0 +1,21 @@
+"""Quiz00 Practice 1"""
+
+# Memory diagram 1
+# a: str = "qwerty"
+# b: str = "3.14"
+# c: str = a[0] + a[len(b)]
+# a = "hehe"
+
+# print(c + b)
+
+# Memory diagram 2
+# a: str = "a"
+# b: str = "a" + "c"
+# print(b + a)
+# b = b + "b"
+# a = a + b[len(a) + 1]
+# print(a[1])
+
+# Expressions 12
+a: str = "the brown, lazy dog!"
+print(a[0] + a[12] + a[5])
